3c7a21f08375e4464cdfbb4bc58b107d3128aa7a
Gihak Kim <kgihak@gmail.com>
2024-10-23 22:08:32 +0200

    Commit and Push Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static final int ScreenWidth = 1280;
+    public static final int ScreenHeight = 720;
+
+    public static void main(String[] args) {
+        new AlienVSEarthling();
+    }
+}
