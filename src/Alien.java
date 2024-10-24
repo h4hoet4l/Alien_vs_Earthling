2fe6aa12226828e7888e2a8eb7007e32bfe4b8f4
Gihak Kim <kgihak@gmail.com>
2024-10-24 16:33:45 +0200

    Import classes that are needed

---
+++ src/Alien.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.util.Random;
+import javax.swing.*
+
+public class Alien {
+    Image image;
+    public int x;
+    public int y;
+    public final int width;
+    public final int height;
+    public int health = 10;
+}
