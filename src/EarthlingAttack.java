f002ec6fe8bb7f029a334eefbf18eb884e3822a6
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-25 16:01:24 +0200

    Add files via upload

---
+++ src/EarthlingAttack.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class EarthlingAttack {
+    public Image image;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public final int attack = 5;
+    static int earthlingColor;
+
+    public EarthlingAttack(int x, int y) {
+        this.x = x;
+        this.y = y;
+        setEarthlingAttackImage();
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
+    private void setEarthlingAttackImage() {
+        switch (earthlingColor) {
+            case 2:
+                image = new ImageIcon("src/images/earthling_attack2.png").getImage();
+                break;
+            case 3:
+                image = new ImageIcon("src/images/earthling_attack3.png").getImage();
+                break;
+            default:
+                image = new ImageIcon("src/images/earthling_attack.png").getImage();
+                break;
+        }
+    }
+}
