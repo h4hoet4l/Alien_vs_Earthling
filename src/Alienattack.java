4efbef44f98c81e73b38cf7b2ce6d9f20c077b81
20203373 <a.j.f.pedro@student.tue.nl>
2024-10-25 03:09:20 +0200

    Add speed, positioning and shoot method

--- src/Alienattack.java
+++ src/Alienattack.java
@@ -6,28 +6,28 @@ public class Alienattack {
     int damage;
     int level;
     ImageIcon appearance;
+    int speed;
+    int x;
+    int y;
 
-    public Attack(int level) {
+    public Alienattack(int x, int y, int level) {
+        this.x = x;
+        this.y = y;
         this.damage = level * 2;
         this.level = level;
         if (level == 1) {
-            this.appearance = new ImageIcon("images/earthling_attack.png", "earthling beam");
+            this.appearance = new ImageIcon("images/alien_attack.png", "alien beam");
+            this.speed = 10;
         } else if (level == 2) {
-            this.appearance = new ImageIcon("images/earthling_attack_double.png", "earthling beam");
+            this.appearance = new ImageIcon("images/alien_attack2.png", "alien beam");
+            this.speed = 12;
         } else {
-            this.appearance = new ImageIcon("images/earthling_attack_triple.png", "earthling beam");
+            this.appearance = new ImageIcon("images/alien_attack3.png", "alien beam");
+            this.speed = 14;
         }
     
     }
-    void update() {
-        if (level == 1) {
-            this.appearance = new ImageIcon("images/earthling_attack.png", "earthling beam");
-        } else if (level == 2) {
-            this.appearance = new ImageIcon("images/earthling_attack_double.png", "earthling beam");
-        } else {
-            this.appearance = new ImageIcon("images/earthling_attack_triple.png", "earthling beam");
-        }
-        this.damage = level * 2;
+    void shoot() {
+        this.x -= speed;
     }
-    
 }
