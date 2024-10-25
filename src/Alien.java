1398c5e975bde1287727106139f051f04c693ff8
20203373 <a.j.f.pedro@student.tue.nl>
2024-10-25 02:09:14 +0200

    Add Alien constructor giving alien appearance and sizing based on alien type

--- src/Alien.java
+++ src/Alien.java
@@ -1,12 +1,26 @@
 import java.awt.*;
 import java.util.Random;
-import javax.swing.*
+import javax.swing.*;
 
 public class Alien {
-    Image image;
+    ImageIcon appearance;
     public int x;
     public int y;
     public final int width;
     public final int height;
     public int health = 10;
+
+    public Alien(int x, int y, boolean level_2) {
+        this.x = x;
+        this.y = y;
+        if (level_2) {
+            this.appearance = new ImageIcon("images/alien2.png", "Advanced alien image");
+            this.width = 400;
+            this.height = 400;
+        } else {
+            this.appearance = new ImageIcon("images/alien1.png", "Advanced alien image");
+            this.width = 500;
+            this.height = 500;
+        }
+    }
 }
