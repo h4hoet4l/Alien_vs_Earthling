2524fe02917d256a1db0398e43320899489b866e
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-25 15:47:06 +0200

    Put back Alien.java as it was deleted accidentally

---
+++ src/Alien.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.util.Random;
+import javax.swing.*;
+
+public class Alien {
+    ImageIcon appearance;
+    public int x;
+    public int y;
+    public final int width;
+    public final int height;
+    public int health = 10;
+    public AlienAttack beam; 
+
+    public Alien(int x, int y, boolean level_2) {
+        this.x = x;
+        this.y = y;
+        if (level_2) {
+            this.appearance = new ImageIcon("src/images/alien2.png", "Advanced alien image");
+            this.width = 400;
+            this.height = 400;
+            this.beam = new AlienAttack(); 
+        } else {
+            this.appearance = new ImageIcon("src/images/alien1.png", "Advanced alien image");
+            this.width = 500;
+            this.height = 500;
+            this.beam = new AlienAttack();
+        }
+    }
+    public void move() {
+
+        this.x -= 7;
+    }
+}
