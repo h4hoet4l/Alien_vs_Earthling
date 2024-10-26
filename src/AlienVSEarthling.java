d2863c45418f20e36eff98bf63cb7da4ac51b86f
20203373 <a.j.f.pedro@student.tue.nl>
2024-10-26 21:02:27 +0200

    Add earthling to 3rd screen and timertest to use for animation of characters

--- src/AlienVSEarthling.java
+++ src/AlienVSEarthling.java
@@ -1,18 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.awt.event.*;
+
+
 
 
 
 public class AlienVSEarthling extends JFrame{
 
     ImageIcon homePage = new ImageIcon(new ImageIcon("src/images/home_page.png", 
-                    "Game main screen").getImage().getScaledInstance(300, 300, Image.SCALE_SMOOTH));
+                    "Game main screen").getImage().getScaledInstance(Main.ScreenWidth, Main.ScreenHeight, Image.SCALE_SMOOTH));
     ImageIcon gameBackground = new ImageIcon(new ImageIcon("src/images/game_page.png", 
-            "Spacey background").getImage().getScaledInstance(600, 600, Image.SCALE_SMOOTH));
+            "Spacey background").getImage().getScaledInstance(Main.ScreenWidth, Main.ScreenHeight, Image.SCALE_SMOOTH));
     ImageIcon loadingScreen = new ImageIcon(new ImageIcon("src/images/loading_page.png",
              "Game loading").getImage().getScaledInstance(600, 600, Image.SCALE_SMOOTH));
     ImageIcon charSelection = new ImageIcon(new ImageIcon("src/images/character_page.png", 
@@ -23,6 +22,7 @@ public class AlienVSEarthling extends JFrame{
     BGM music = new BGM("src/audio/MainPageBGM.wav", true);
     Earthling earthling;
     
+    
     public AlienVSEarthling() {
         
         setSize(Main.ScreenWidth, Main.ScreenHeight);
@@ -30,6 +30,7 @@ public class AlienVSEarthling extends JFrame{
         setVisible(true);
         setTitle("Aliens vs Earthlings");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
         music.start();
         addKeyListener(new KeyListener());
         
@@ -61,20 +62,31 @@ public class AlienVSEarthling extends JFrame{
     public void startGame(){
         changeCurrentScreen(gameBackground, false);
         changeMusic(new BGM("src/audio/FirstRoundBGM.wav", true));
+        drawGame(this.getGraphics());
+        
     }
 
     void chooseEarthling(int earthling) {
     	this.earthling = new Earthling(0, 0, earthling);
     }
 
-
+    public void drawGame(Graphics g) {
+        this.remove(screenImage);
+        EarthlingPanel drawnEarthling = new EarthlingPanel();
+        this.add(drawnEarthling);
+        
+        //this.pack();
+    }
+    public void updateScreen() {
+        
+    }
     
 
     class KeyListener extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
                 case KeyEvent.VK_1:
-                    if(currentScreen == charSelection) {
+                    if (currentScreen == charSelection) {
                         chooseEarthling(0);
                         startGame();
                     }
@@ -87,9 +99,9 @@ public class AlienVSEarthling extends JFrame{
                     break;
                 case KeyEvent.VK_3:
                     if (currentScreen == charSelection) {
-                    	chooseEarthling(2);
+                        chooseEarthling(2);
                         startGame();
-                    	EarthlingAttack.earthlingColor = 3;  
+                        EarthlingAttack.earthlingColor = 3;
                     }
                     break;
                 case KeyEvent.VK_ENTER:
@@ -97,8 +109,50 @@ public class AlienVSEarthling extends JFrame{
                         changeCurrentScreen(charSelection, false);  
                     }
                     break;
-                
+                case KeyEvent.VK_W:
+                    if (currentScreen == gameBackground) {
+                        earthling.move('w');  
+                    }
+                    break;
+                case KeyEvent.VK_D:
+                    if (currentScreen == gameBackground) {
+                        earthling.move('d');  
+                    }
+                    break;
+                case KeyEvent.VK_S:
+                    if (currentScreen == gameBackground) {
+                        earthling.move('s');  
+                    }
+                    break;
+                case KeyEvent.VK_A:
+                    if (currentScreen == gameBackground) {
+                        earthling.move('a');  
+                    }
+                    break;
             }
         }
+    
 }
+class EarthlingPanel extends JPanel {
+    @Override
+    public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.drawImage(gameBackground.getImage(), 0, 0, null);
+    g.drawImage(earthling.getImage(), earthling.getCoordinates()[0],earthling.getCoordinates()[1], null);
+}
+}
+class TimerTest implements ActionListener {
+    Timer timer = new Timer(1000, this);
+    void run() {
+    timer.start();
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == timer) {
+        updateScreen();
+        System.out.print("b");
+        } else {
+        System.out.print(" . ");
+        }
+        }
+    }
 }
