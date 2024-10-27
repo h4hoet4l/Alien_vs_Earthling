bd3b86785ffe53ffe6e0512a7d30d6c1dedcd6d5
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-27 20:47:58 +0100

    Added JavaDoc

--- src/Alien.java
+++ src/Alien.java
@@ -2,6 +2,10 @@ import java.awt.*;
 import java.util.Random;
 import javax.swing.*;
 
+/**
+ * Represents an alien in a game. The alien can move and has an appearance,
+ * health, and an associated attack beam.
+ */
 public class Alien {
     Image appearance;
     public int x;
@@ -11,14 +15,24 @@ public class Alien {
     public int health = 10;
     public AlienAttack beam; 
 
-    public Alien (int x, int y) {
+    /**
+     * Constructs an Alien object with the specified position.
+     *
+     * @param x the x-coordinate of the alien's initial position
+     * @param y the y-coordinate of the alien's initial position
+     */
+    public Alien(int x, int y) {
         this.x = x;
         this.y = y;
         setAlienImage();
         width = appearance.getWidth(null);
         height = appearance.getHeight(null);
     }
-    
+
+    /**
+     * Sets the appearance of the alien randomly, choosing between two images,
+     * and initializes the alien's attack beam based on the chosen image.
+     */
     private void setAlienImage() {
         Random random = new Random();
         int randNum = random.nextInt(100);
@@ -34,11 +48,19 @@ public class Alien {
         }
     }
 
-
+    /**
+     * Moves the alien left on the screen by a fixed amount.
+     * This method updates the alien's x-coordinate.
+     */
     public void move() {
         this.x -= 10;
     }
 
+    /**
+     * Returns the current coordinates of the alien as an array.
+     *
+     * @return an array containing the x and y coordinates of the alien
+     */
     public int[] getCoordinates() {
         int[] coordinates = {x, y};
         return coordinates;
