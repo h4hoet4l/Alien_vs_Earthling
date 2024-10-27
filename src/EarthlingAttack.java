0949cc1bf0b0839757bd8018e6808b0e435092ac
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-27 20:50:48 +0100

    Added Javadoc

--- src/EarthlingAttack.java
+++ src/EarthlingAttack.java
@@ -1,15 +1,26 @@
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Represents an Earthling's attack in the game, including attributes such as
+ * position, appearance, attack strength, and functionality for moving the attack.
+ */
 public class EarthlingAttack {
     public Image image;
     public int x;
     public int y;
     public int width;
     public int height;
-    public final int attack = 5;
+    public final int attack = 3;
     static int earthlingColor;
 
+    /**
+     * Constructs an EarthlingAttack at specified coordinates with an image 
+     * based on the Earthling's color.
+     * 
+     * @param x the initial x-coordinate of the attack
+     * @param y the initial y-coordinate of the attack
+     */
     public EarthlingAttack(int x, int y) {
         this.x = x;
         this.y = y;
@@ -18,13 +29,21 @@ public class EarthlingAttack {
         height = image.getHeight(null);
     }
 
+    /**
+     * Sets the image for the Earthling's attack based on the color specified by earthlingColor.
+     * Images vary according to different earthling types.
+     */
     private void setEarthlingAttackImage() {
         image = switch (earthlingColor) {
             case 2 -> new ImageIcon("src/images/earthling_attack2.png").getImage();
             case 3 -> new ImageIcon("src/images/earthling_attack3.png").getImage();
             default -> new ImageIcon("src/images/earthling_attack.png").getImage();
         };
-    }        
+    }
+        
+    /**
+     * Moves the Earthling's attack in the rightward direction, simulating the attack action.
+     */
     public void shoot() {
         this.x += 15;
     }
