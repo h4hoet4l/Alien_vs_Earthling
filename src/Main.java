307f8c4c404486ccfe78fb962d81dd7bd174c818
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-27 20:51:48 +0100

    Added Javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+/**
+ * The Main class that serves as the entry point for the Alien vs Earthling game.
+ * It initializes the game window and starts the game.
+ */
 public class Main {
-    
-    public static final int ScreenWidth = 1280;
-    public static final int ScreenHeight = 720;
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
 
     public static void main(String[] args) {
         new AlienVSEarthling();
