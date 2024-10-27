0fd0c83d5644ab57c26f9e9ce9c6d973f9457ca0
Gihak Kim <87720153+Gihak@users.noreply.github.com>
2024-10-27 20:49:52 +0100

    Added Javadoc

--- src/BGM.java
+++ src/BGM.java
@@ -2,12 +2,22 @@ import java.io.File;
 import java.io.IOException;
 import javax.sound.sampled.*;
 
+/**
+ * Represents background music (BGM) for the game. This class manages
+ * loading, starting, and stopping audio clips.
+ */
 public class BGM {
     public Clip clip;
     public File audioFile;
     public AudioInputStream audioInputStream;
     public boolean isLoop;
 
+    /**
+     * Constructs a BGM object with the specified audio file path and looping behavior.
+     *
+     * @param pathName the path to the audio file to be played
+     * @param isLoop a boolean indicating whether the audio should loop continuously
+     */
     public BGM(String pathName, boolean isLoop) {
         try {
             clip = AudioSystem.getClip();
@@ -22,7 +32,11 @@ public class BGM {
             e.printStackTrace();
         }
     }
-
+    
+    /**
+     * Starts playing the background music from the beginning.
+     * If the isLoop flag is set to true, the music will loop continuously.
+     */
     public void start() {
         clip.setFramePosition(0);
         clip.start();
